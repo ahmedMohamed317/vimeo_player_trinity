64875f05e498231f5630014e895eced3bdaee15a
elite <ahmed.momorad317@gmail.com>
2024-09-07 19:48:26 +0300

    api_modify_token

--- lib/src/quality_links.dart
+++ lib/src/quality_links.dart
@@ -1,12 +1,10 @@
-import 'dart:developer';
-
-import 'package:http/http.dart' as http;
 import 'dart:convert';
-import 'dart:async';
-import "dart:collection";
+import 'dart:collection';
+import 'package:http/http.dart' as http;
 
 class QualityLinks {
   String? videoId;
+  final String accessToken = "ba1f80c3c84fd5409c7e68f612ce8322";
 
   QualityLinks(this.videoId);
 
@@ -16,43 +14,55 @@ class QualityLinks {
 
   Future<SplayTreeMap?> getQualitiesAsync() async {
     try {
-      final vimeoLink =
-          Uri.tryParse('https://api.vimeo.com/videos/${videoId}');
-      var response = await http.get(vimeoLink!);
+      final vimeoLink = Uri.tryParse('https://api.vimeo.com/videos/$videoId');
 
-      final jsonData = jsonDecode(response.body)['request']['files'];
-      final dashData = jsonData['dash'];
-      final hlsData = jsonData['hls'];
-      final defaultCDN = hlsData['default_cdn'];
-      final cdnVideoUrl = (hlsData['cdns'][defaultCDN]['url'] as String?) ?? '';
-      final rawStreamUrls =
-          (dashData['streams'] as List<dynamic>?) ?? <dynamic>[];
+      var response = await http.get(
+        vimeoLink!,
+        headers: {
+          'Authorization': 'Bearer $accessToken',
+        },
+      );
 
-      final sepList = cdnVideoUrl.split('/sep/video/');
-      final firstUrlPiece = sepList.firstOrNull ?? '';
-      final lastUrlPiece = ((sepList.lastOrNull ?? '').split('/').lastOrNull) ??
-          (sepList.lastOrNull ?? '');
+      if (response.statusCode == 200) {
+        final jsonData = jsonDecode(response.body)['request']['files'];
+        final dashData = jsonData['dash'];
+        final hlsData = jsonData['hls'];
+        final defaultCDN = hlsData['default_cdn'];
+        final cdnVideoUrl =
+            (hlsData['cdns'][defaultCDN]['url'] as String?) ?? '';
+        final rawStreamUrls =
+            (dashData['streams'] as List<dynamic>?) ?? <dynamic>[];
 
-      final SplayTreeMap videoList = SplayTreeMap();
+        final sepList = cdnVideoUrl.split('/sep/video/');
+        final firstUrlPiece = sepList.firstOrNull ?? '';
+        final lastUrlPiece =
+            ((sepList.lastOrNull ?? '').split('/').lastOrNull) ??
+                (sepList.lastOrNull ?? '');
 
-      for (final item in rawStreamUrls) {
-        final urlId =
-            ((item['id'] ?? '') as String).split('-').firstOrNull ?? '';
+        final SplayTreeMap videoList = SplayTreeMap();
 
-        videoList.putIfAbsent(
-          "${item['quality']} ${item['fps']}",
-          () => '$firstUrlPiece/sep/video/$urlId/$lastUrlPiece',
-        );
+        for (final item in rawStreamUrls) {
+          final urlId =
+              ((item['id'] ?? '') as String).split('-').firstOrNull ?? '';
+
+          videoList.putIfAbsent(
+            "${item['quality']} ${item['fps']}",
+                () => '$firstUrlPiece/sep/video/$urlId/$lastUrlPiece',
+          );
+        }
+
+        if (videoList.isEmpty) {
+          videoList.putIfAbsent(
+            '720 30',
+                () => cdnVideoUrl,
+          );
+        }
+
+        return videoList;
+      } else {
+        print('=====> ERROR: ${response.statusCode}: ${response.body}');
+        return null;
       }
-
-      if (videoList.isEmpty) {
-        videoList.putIfAbsent(
-          '720 30',
-          () => cdnVideoUrl,
-        );
-      }
-
-      return videoList;
     } catch (error) {
       print('=====> REQUEST ERROR: $error');
       return null;
